1c5b8698e0607ccb96859b07cb627f896830596e
Nadyita <nadyita@hodorraid.org>
2022-08-13 10:23:36 +0200

    Move to 6.1.0 syntax

--- NameGeneratorController.php
+++ NameGeneratorController.php
@@ -2,15 +2,21 @@
 
 namespace Nadybot\User\Modules\NAME_GENERATOR;
 
+use function Amp\call;
+use Amp\Http\Client\{HttpClientBuilder, Request, Response};
+use Amp\Promise;
+use Generator;
 use Illuminate\Support\Collection;
+use Nadybot\Core\DBSchema\Player;
+
+use Nadybot\Core\Modules\PLAYER_LOOKUP\PlayerManager;
 use Nadybot\Core\{
 	Attributes as NCA,
 	CmdContext,
 	ModuleInstance,
-	Http,
-	HttpResponse,
+	UserException,
 };
-use Nadybot\Core\Modules\PLAYER_LOOKUP\PlayerManager;
+use Throwable;
 
 /**
  * A command to generate new character names.
@@ -20,26 +26,19 @@ use Nadybot\Core\Modules\PLAYER_LOOKUP\PlayerManager;
 #[
 	NCA\Instance,
 	NCA\DefineCommand(
-		command:     'suggestname',
+		command: 'suggestname',
 		accessLevel: 'all',
 		description: 'Generate a random character name',
 	)
 ]
 
 class NameGeneratorController extends ModuleInstance {
-	#[NCA\Inject()]
-	public PlayerManager $playerManager;
-
-	#[NCA\Inject()]
-	public Http $http;
-
-	/**
-	 * The URL to the name generator webpage with the name length as a parameter
-	 */
+	/** The URL to the name generator webpage with the name length as a parameter */
 	public const NAME_GENERATOR_URL = 'https://www.fantasynamegen.com/sf/%s/';
 
 	/**
 	 * An array of all valid lengths
+	 *
 	 * @var string[] LENGTHS
 	 */
 	public const LENGTHS = [
@@ -47,9 +46,15 @@ class NameGeneratorController extends ModuleInstance {
 		'medium',
 		'long',
 	];
+	#[NCA\Inject()]
+	public PlayerManager $playerManager;
+
+	#[NCA\Inject()]
+	public HttpClientBuilder $http;
 
 	/**
 	 * Try to parse the array of generated names from the name generator HTML
+	 *
 	 * @return string[]
 	 */
 	public function nameGeneratorHtmlToNames(string $html): array {
@@ -57,53 +62,60 @@ class NameGeneratorController extends ModuleInstance {
 		return $matches[1];
 	}
 
-	/**
-	 * Generate a character name with an optional length
-	 */
+	/** Generate a character name with an optional length */
 	#[NCA\HandlesCommand("suggestname")]
 	public function nameCommand(
 		CmdContext $context,
 		#[NCA\StrChoice("short", "medium", "long")] ?string $length
-	): void {
+	): Generator {
 		$length ??= static::LENGTHS[array_rand(static::LENGTHS)];
-		$this->http
-				->get(sprintf(static::NAME_GENERATOR_URL, $length))
-				->withTimeout(5)
-				->withCallback([$this, "sendName"], $context);
+		$freeNames = yield $this->getFreeNames(strtolower($length));
+		$msg = $this->renderNameSuggestions(...$freeNames);
+		$context->reply($msg);
 	}
 
-	public function sendName(HttpResponse $response, CmdContext $context): void {
-		if ($response->error) {
-			$context->reply($response->error);
-			return;
-		}
-		$names = $this->nameGeneratorHtmlToNames($response->body ?? "");
-		if (empty($names)) {
-			$msg = "No names were found. If this occurs too often, please contact the author of the module.";
-			$context->reply($msg);
-			return;
-		}
+	/** @return Promise<string[]> */
+	private function getFreeNames(string $length): Promise {
+		return call(function () use ($length): Generator {
+			$client = $this->http->buildDefault();
 
-		$this->playerManager->massGetByNameAsync(
-			/** @param string[] $names */
-			function (array $names) use ($context): void {
-				$this->showNameSuggestions($names, $context);
-			},
-			$names
-		);
+			try {
+				/** @var Response */
+				$response = yield $client->request(new Request(sprintf(static::NAME_GENERATOR_URL, $length)));
+				$body = yield $response->getBody()->buffer();
+			} catch (Throwable $e) {
+				throw new UserException("Unexpected error calling the name API. Try again later, or come up with your own name.");
+			}
+			$names = $this->nameGeneratorHtmlToNames($body);
+			if (empty($names)) {
+				throw new UserException("No names were found. If this occurs too often, please contact the author of the module.");
+			}
+			$lookups = [];
+			foreach ($names as $name) {
+				$lookups[$name] = $this->playerManager->byName($name);
+			}
+
+			/** @var array<string,?Player> */
+			$results = yield $lookups;
+
+			/** @var string[] */
+			$freeNames = array_keys(
+				array_filter(
+					$results,
+					fn (?Player $data): bool => $data === null,
+				)
+			);
+			return $freeNames;
+		});
 	}
 
-	/** @param string[] $names */
-	protected function showNameSuggestions(array $names, CmdContext $context): void {
-		$names = (new Collection($names))
-			->filter(fn(?object $data): bool => $data === null)
-			->keys();
+	private function renderNameSuggestions(string ...$names): string {
+		$names = new Collection($names);
 		if ($names->isEmpty()) {
-			$context->reply("No unused names found, please try again.");
-			return;
+			return "No unused names found, please try again.";
 		}
 		$msg = "Name suggestions for your next character: <highlight>".
 			$names->join("<end>, <highlight>", "<end> or <highlight>") . "<end>.";
-		$context->reply($msg);
+		return $msg;
 	}
 }
